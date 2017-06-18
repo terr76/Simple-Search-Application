a94ac2ff9138d669a6424cdd6e9a598c355e209f
Kibeom Kim <terr76.kim@gmail.com>
2017-06-17 21:57:40 -0400

    Add DB install files

---
+++ _install/02-create-table-song.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ _install/03-insert-demo-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 'John', 'john@example.com', '$2y$10$3Gk5nKC0FXtiwxZJ1qLwnutd1gzEmZtvFfjXtw0CsG4JBBHk/vj7e'),
+(2, 'Jane', 'jane@example.com', '$2y$10$nifTQ6C.2kpo9HlqnlOXFOHTcc9kVZmRdi0R5K52TnpxxkkgYAZkG');
